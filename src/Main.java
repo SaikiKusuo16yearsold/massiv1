d238f30615390b7b0f74b4e752807af1487e49d7
имя фамилия <Dima>
2024-10-15 23:55:18 +0300

    finall commit

--- src/Main.java
+++ src/Main.java
@@ -1,79 +1,17 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        //задача 1
-        byte[] three = new byte[3];
-        three[0] = 1;
-        three[1] = 2;
-        three[2] = 3;
+        Author writerDima = new Author("dima", "davydov");
+        Author writerSasha = new Author("sasha", "eblanov");
+        Book firstBook = new Book("Voina i Mir", writerDima, 1984);
+        Book secondBook = new Book("Prestuplenie i nakazanie", writerSasha, 1978);
 
-        float[] fractionalNumbers = {1.57F, 7.654F, 9.986F};
+        System.out.println(secondBook.getYearPublication());
+        secondBook.setYearPublication(1967);
+        System.out.println(secondBook.getYearPublication());
+        System.out.println(writerDima.getName());
+        System.out.println(writerDima.getFamily());
 
-        int[] age = {14, 15, 16};
-        //задача 2
-        for (int i = 0; i <= three.length - 1; i++) {
-            if (i != three.length - 1) {
-                System.out.printf(three[i] + ", ");
-            } else {
-                System.out.println(three[i]);
-            }
 
-        }
-        for (int i = 0; i <= fractionalNumbers.length - 1; i++) {
-            if (i != fractionalNumbers.length - 1) {
-                System.out.printf(fractionalNumbers[i] + ", ");
-            } else {
-                System.out.println(fractionalNumbers[i]);
-            }
-        }
-        for (int i = 0; i <= age.length - 1; i++) {
-            if (i != age.length - 1) {
-                System.out.printf(age[i] + ", ");
-            } else {
-                System.out.println(age[i]);
-            }
-        }
-        //задча 3
-        for (int i = three.length - 1; 0 <= i; i--) {
-            if (i != 0) {
-                System.out.printf(three[i] + ", ");
-            } else {
-                System.out.println(three[i]);
-            }
-
-        }
-        for (int i = fractionalNumbers.length - 1; 0 <= i; i--) {
-            if (i != 0) {
-                System.out.printf(fractionalNumbers[i] + ", ");
-            } else {
-                System.out.println(fractionalNumbers[i]);
-            }
-
-        }
-        for (int i = age.length - 1; 0 <= i; i--) {
-            if (i != 0) {
-                System.out.printf(age[i] + ", ");
-            } else {
-                System.out.println(age[i]);
-            }
-
-        }
-
-        //задача 4
-        for (int i = 0; i <= three.length - 1; i++) {
-            if (three[i] % 2 != 0) {
-                three[i] += 1;
-            }
-        }
-        //результат преобразования
-        System.out.println(Arrays.toString(three));
     }
-
-
 }
-
-
-
-
